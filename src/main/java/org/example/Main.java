6ec32bdab1a6edcb1f38c7e84cced7f2f1282951
lovmint <sna135246@gmail.com>
2023-09-01 16:51:20 +0900

    Create Project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("2210000", "홍길동");
+        System.out.println("Hello world!");
+        System.out.println("s1.toString()");
+    }
+}
